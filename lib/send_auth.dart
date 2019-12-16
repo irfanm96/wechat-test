155857f4a1aa1b788b55b4507f3a6255df27a9ec
Mohamed Fawzan <fawzanm@gmail.com>
2019-12-16 12:56:45 +0530

    Emergency Updates

--- lib/send_auth.dart
+++ lib/send_auth.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fluwx/fluwx.dart' as fluwx;
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 class SendAuthPage extends StatefulWidget {
   @override
@@ -8,16 +10,28 @@ class SendAuthPage extends StatefulWidget {
 
 class _SendAuthPageState extends State<SendAuthPage> {
   String _result = "无";
+  String APP_ID = 'wx52be4f5f1e383173';
+  String APP_SECRET = '946125d07a2c3c5406873210c14185f8';
 
   @override
   void initState() {
     super.initState();
-    fluwx.responseFromAuth.listen((data) {
+
+    fluwx.responseFromAuth.listen((data) async {
       print(data.code);
+
+      var url =
+          'https://api.weixin.qq.com/sns/oauth2/access_token?appid=${APP_ID}&secret=${APP_SECRET}&code=${data.code}&grant_type=authorization_code';
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        var jsonResponse = convert.jsonDecode(response.body);
+        print(jsonResponse);
+      } else {
+        print("Request failed with status: ${response.statusCode}.");
+      }
 
       setState(() {
         _result = "${data.errCode}";
-
       });
     });
   }
@@ -40,10 +54,9 @@ class _SendAuthPageState extends State<SendAuthPage> {
             onPressed: () {
               fluwx
                   .sendAuth(
-                  scope: "snsapi_userinfo", state: "wechat_sdk_demo_test")
+                      scope: "snsapi_userinfo", state: "wechat_sdk_demo_test")
                   .then((data) {
-                    print(data);
-
+                print(data);
               });
             },
             child: const Text("send auth"),
